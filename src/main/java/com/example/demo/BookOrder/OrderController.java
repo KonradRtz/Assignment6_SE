98ca81a42e502edc34194320758cdc49d1d1ea2e
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2023-06-02 20:00:10 +0200

    changed order constructor

--- src/main/java/com/example/demo/BookOrder/OrderController.java
+++ src/main/java/com/example/demo/BookOrder/OrderController.java
@@ -1,17 +1,4 @@
 package com.example.demo.BookOrder;
-<<<<<<< Updated upstream
-
-import java.util.ArrayList;
-
-public class OrderController {
-    
-    private ArrayList<Order> orders;
-
-    public void orderBook(Book book, int orderQuantity){
-        Order currentOrder = new Order(book,orderQuantity, String.valueOf(System.currentTimeMillis()));
-        orders.add(currentOrder);
-        // when the order is placed also remove this quantity through the api
-=======
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,20 +12,12 @@ public class OrderController {
     private ArrayList<Order> orders = new ArrayList<>();
 
     @PostMapping("/bookorder")
-    public void orderBook(String bookId, int orderQuantity, String date){
-        Order newOrder = new Order(bookId, orderQuantity, date);
+    public void orderBook(Book book, int orderQuantity, String date){
+        Order newOrder = new Order(book, orderQuantity, date);
         orders.add(newOrder);
->>>>>>> Stashed changes
     }
 
     public ArrayList<Order> getOrders(){
         return orders;
     }
-<<<<<<< Updated upstream
-
-    public OrderController(){
-        orders = new ArrayList<>();
-    }
-=======
->>>>>>> Stashed changes
 }
